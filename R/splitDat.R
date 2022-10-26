458d248bb81b41a9c46c519038070e971bcf72ec
Ningyuan Xu <xu.ningyuan@outlook.com>
2022-10-25 20:58:13 -0700

    my commit message

--- R/splitDat.R
+++ R/splitDat.R
@@ -4,14 +4,15 @@
 #' where the training set will contain 'ntrain' of examples
 #' 'ntrain' must be smaller or equal to the total size of 'data'
 #'
-#' @param 'data' is a data frame or tbl for the dataset
-#' 'ntrain' will be numeric, 'seed' will be the numeric
+#' @param data is a data frame or tbl for the dataset
+#' @param ntrain will be numeric as the size of the training set
+#' @param seed will be the numeric to generate randomness
 #'
 #' @return a list containing both training and holdout sets
 #' @export
 #'
 #' @examples
-#' split_dataset(datateachr::cancer_sample, 300, 447)
+#' split_dataset(cars, 30, 447)
 split_dataset <- function(data, ntrain, seed) {
   stopifnot(is.numeric(ntrain))
   stopifnot(is.numeric(seed))
--- tests/testthat/test-splitDat.R
+++ tests/testthat/test-splitDat.R
@@ -1,11 +1,10 @@
-library(datateachr)
 test_that("Test size of training set in valid cases", {
-  expect_equal(nrow(split_dataset(cancer_sample, 300, 447)[[1]]), 300)
+  expect_equal(nrow(split_dataset(cars, 30, 447)[[1]]), 30)
 })
 test_that("Test size of hold-out set in valid cases", {
-  expect_equal(nrow(split_dataset(cancer_sample, 300, 321)[[2]]),
-               nrow(cancer_sample) - 300)
+  expect_equal(nrow(split_dataset(cars, 30, 321)[[2]]),
+               nrow(cars) - 30)
 })
 test_that("Test the return types in valid cases", {
-  expect_equal(class(split_dataset(cancer_sample, 300, 321)), "list")
+  expect_equal(class(split_dataset(cars, 30, 321)), "list")
 })
