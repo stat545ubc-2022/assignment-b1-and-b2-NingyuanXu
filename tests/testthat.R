ab91fa31895f5284aa4529b8827642b57bf6a505
Ningyuan Xu <xu.ningyuan@outlook.com>
2022-10-25 19:50:02 -0700

    almost done

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,7 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(datateachr)
 library(splitDat)
 
 test_check("splitDat")
--- tests/testthat/test-splitDat.R
+++ tests/testthat/test-splitDat.R
@@ -1,3 +1,4 @@
+library(datateachr)
 test_that("Test size of training set in valid cases", {
   expect_equal(nrow(split_dataset(cancer_sample, 300, 447)[[1]]), 300)
 })
